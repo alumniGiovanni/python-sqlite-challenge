495fc73f08a048843f3a50eae22e669409097b73
Giovanni <giovanni.andrade9991@gmail.com>
2022-03-13 00:44:06 -0300

    Fetching data with a where clause to filter by year

--- sqlite-challenge.py
+++ sqlite-challenge.py
@@ -6,5 +6,20 @@ cursor = connection.cursor()
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS filmes (nome TEXT, Diretor TEXT, Ano INT)''')
 
+filmesData = [('Pulp Fiction', 'Quentin Tarantino', 1994),
+('Back to the future', 'Steven Spielberg', 1985),
+('Avengers', 'Marvel', 2014)]
+
+cursor.executemany('Insert INTO filmes VALUES (?,?,?)', filmesData)
+
+#valores = cursor.execute("SELECT * from filmes")
+
+anoLancamento = (2014,)
+
+#print(cursor.fetchall())
+cursor.execute("SELECT * FROM filmes WHERE ano=?", anoLancamento)
+
+print(cursor.fetchall())
+
 connection.commit
 connection.close
